a70b264505ecc7c9bd0034ee27fcf9259a730d39
Kleyson Mariano <kleysons913@gmail.com>
2023-07-26 21:53:37 -0300

    First Commit

---
+++ ScreenSound/Musica.cs
@@ -0,0 +1,24 @@
+﻿using System.Xml;
+
+class Musica
+{
+    public string nome;
+    public string artista;
+    public int duracao;
+    public bool disponivel;
+
+    public void ExibirFichaTecnica()
+    {
+        Console.WriteLine($"Nome: {nome}");
+        Console.WriteLine($"Artista: {artista}");
+        Console.WriteLine($"Duração: {duracao}");
+        if (disponivel)
+        {
+            Console.WriteLine("Disponivel no plano.");
+        }
+        else
+        {
+            Console.WriteLine("Adquira o plano Plus+");
+        }
+    }   
+}
