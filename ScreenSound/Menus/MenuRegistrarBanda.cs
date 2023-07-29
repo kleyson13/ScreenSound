56e47ebe29e338aacd13f9500db03142de41ba52
Kleyson Mariano <kleysons913@gmail.com>
2023-07-28 23:46:52 -0300

    Integrando ao ChatGPT

--- ScreenSound/Menus/MenuRegistrarBanda.cs
+++ ScreenSound/Menus/MenuRegistrarBanda.cs
@@ -1,4 +1,5 @@
-﻿using ScreenSound.Modelos;
+﻿using OpenAI_API;
+using ScreenSound.Modelos;
 
 namespace ScreenSound.Menus;
 
@@ -12,8 +13,19 @@ internal class MenuRegistrarBanda : Menu
         string nomeDaBanda = Console.ReadLine()!;
         Banda banda = new Banda(nomeDaBanda);
         bandasRegistradas.Add(nomeDaBanda, banda);
+
+        var client = new OpenAIAPI("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");
+
+        var chat = client.Chat.CreateConversation();
+
+        chat.AppendSystemMessage($"Resuma a banda {nomeDaBanda} em 1 parágrafo breve. Adote um estilo Informal.");
+
+        string resposta = chat.GetResponseFromChatbotAsync().GetAwaiter().GetResult();
+        banda.Resumo = resposta;
+
         Console.WriteLine($"A banda {nomeDaBanda} foi registrada com sucesso!");
-        Thread.Sleep(4000);
+        Console.WriteLine("\nDigite uma tecla para voltar ao menu principal");
+        Console.ReadKey();
         Console.Clear();
     }
 }
